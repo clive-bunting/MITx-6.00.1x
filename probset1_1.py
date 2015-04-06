3a9cb63c8b353c39f749a53d4cf9c1c497dbd68a
Clive Bunting <clive.bunting@googlemail.com>
2015-04-06 14:27:59 +0100

    Removing moved files

--- probset1_1.py
+++
@@ -1,8 +0,0 @@
-s = 'azcbobobegghakl'
-
-count = 0
-for letter in s:
-    if (letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u'):
-        count += 1
-print 'Number of vowels:',
-print count
